58f78781e8974a91b69703589e28fea37be5650a
Mohamedmoussa19999 <1701297@eng.asu.edu.eg>
2021-09-30 23:05:13 +0200

    handle no user scenario

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,25 +1,52 @@
-import React, { useContext } from 'react';
+import React, { useContext,useState , useEffect } from 'react';
 import { UserContext } from '../App';
 import './scss/navbar.scss';
-
+let loggin = false;
 const NavBar = () => {
   const authUser = useContext(UserContext);
+  const [user, setUser] = useState('inline-block');
+  const [guest, setGuest] = useState('none');
+  useEffect(() => {
+    if (loggin) {
+      setUser('inline-block')
+      setGuest('none')
+    }
+    else {
+      setUser('none')
+      setGuest('inline-block')
+    }
+  });
+  
   return (
     <header className="header">
       <div className="header__logo">Hire Mi</div>
       <nav className="header__navbar">
-        <ul className="header__navbar__ul">
-          <li className="header__navbar__ul__li">
-            {authUser.firstName} {authUser.lastName}
-          </li>
-          <li className="header__navbar__ul__li">
-            <img
-              className="user-avatar"
-              src={authUser.avatarURL}
-              alt="avatar"
-            />
-          </li>
-        </ul>
+        
+          <ul className="header__navbar__ul" style={{display:user}} >
+            <li className="header__navbar__ul__li">
+              {authUser.firstName} {authUser.lastName}
+            </li>
+            <li className="header__navbar__ul__li">
+              <img
+                className="user-avatar"
+                src={authUser.avatarURL}
+                alt="avatar"
+              />
+            </li>
+          </ul>
+        
+          <ul className="guest__ul" style={{display:guest}}>
+            <li className="guest__ul__li__login">
+              login
+            </li>
+            <li className="guest__ul__li__signup">
+              Signup
+            </li>
+            <li className="guest__ul__li__interview">
+              Take Interview
+            </li>
+          </ul>
+        
       </nav>
     </header>
   );
