eee83c444608e6ac738204382455862acfe2d6e6
mo1998 <mo.181298@gmail.com>
2022-02-18 19:55:57 +0200

    Added script to run light detection

---
+++ models/models/lightDetectScript.py
@@ -0,0 +1,26 @@
+import asyncio
+import websockets
+import time
+from model import lightFaceDetect3
+
+async def echo(websocket):
+    async for message in websocket:
+        if not isinstance(message, str):
+            detection_model = lightFaceDetect3()
+            result = detection_model.detection(message)
+            t = time.time()
+            print(t)
+            print(type(message), result)
+            with open(f'test-{t}.png', "wb") as out_file:
+                out_file.write(message)
+
+
+        else:
+            print(message)
+            await websocket.send(message)
+
+async def main():
+    async with websockets.serve(echo, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
