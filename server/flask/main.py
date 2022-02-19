3990be590199a6dc8108051435d29312e0d4aef3
MedoOnlyOne <mohamed.medhat2199@gmail.com>
2022-02-19 13:42:56 +0200

    Timeout fix for webSocket

--- server/flask/main.py
+++ server/flask/main.py
@@ -5,19 +5,21 @@ from model import lightFaceDetect3
 
 async def echo(websocket):
     async for message in websocket:
-        if not isinstance(message, str):
-            detection_model = lightFaceDetect3()
-            result = detection_model.detection(message)
-            t = time.time()
-            print(type(message), result, t)
-            await websocket.send(str(result))
-            # with open(f'test-{t}.png', "wb") as out_file:
-            #     out_file.write(message)
-
-
-        else:
-            print(message)
-            await websocket.send(message)
+        try:
+            if not isinstance(message, str):
+                detection_model = lightFaceDetect3()
+                result = detection_model.detection(message)
+                t = time.time()
+                print(type(message), result, t)
+                await websocket.send(str(result))
+                # with open(f'test-{t}.png', "wb") as out_file:
+                #     out_file.write(message)
+            else:
+                print(message)
+                await websocket.send(message)
+        
+        except websockets.ConnectionClosed as e:
+            print(f'Timeout', e)
 
 async def main():
     async with websockets.serve(echo, "localhost", 8765):
