8cfece2d57c35162feae774c69eafbe949e5a1b1
Mohamedmoussa19999 <1701297@eng.asu.edu.eg>
2022-03-31 00:32:57 +0200

    Showing ML results

--- frontend/src/components/EvaluationCard.js
+++ frontend/src/components/EvaluationCard.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from './Card';
 import './scss/evaluate.scss';
+import { Bar } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
+
 const EvaluationCard = React.forwardRef((props, ratings) => {
-  console.log(props.answers);
+  let emotionLabels = ['happy', 'sad', 'angry', 'surprise', 'neutral'];
+  const getEmotions = (emotions, index) => {
+    let emotion = [
+      emotions.happy * 100,
+      emotions.sad * 100,
+      emotions.angry * 100,
+      emotions.surprise * 100,
+      emotions.neutral * 100,
+    ];
+    let chartData = [];
+    chartData.push({
+      labels: emotionLabels.map((label) => label),
+      datasets: [
+        {
+          data: emotion.map((emotion) => emotion),
+          backgroundColor: ['red', 'grey', 'green', 'yellow', 'purple'],
+        },
+      ],
+    });
+    return (
+      <div>
+        <Bar
+          data={chartData[index]}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: 'Emotions Results',
+              },
+              legend: {
+                display: false,
+              },
+            },
+          }}
+        />
+      </div>
+    );
+  };
   return (
-    <div ref={ratings}>
+    <div>
       <ul>
         {props.answers.map((answer, index) => (
           <li key={index}>
@@ -12,6 +54,23 @@ const EvaluationCard = React.forwardRef((props, ratings) => {
               <p className="question-title">
                 Question{index + 1}: {answer.statement}
               </p>
+              {getEmotions(answer.emotions, index)}
+              <div className="dataContainer">
+                <p htmlFor="email" className="detailsLabel">
+                  OpenPose:
+                </p>
+                <p name="email" className="info">
+                  {answer.openPose}
+                </p>
+              </div>
+              <div className="dataContainer">
+                <p htmlFor="email" className="detailsLabel">
+                  Score:
+                </p>
+                <p name="email" className="info">
+                  {answer.score}
+                </p>
+              </div>
               <div className="evaluate_video">
                 <video width="83%" height="250" controls>
                   <source src={answer.link} />
@@ -22,6 +81,8 @@ const EvaluationCard = React.forwardRef((props, ratings) => {
                 type="text"
                 placeholder="Rate from 0% to 100%"
                 className="evaluation-rate"
+                ref={ratings}
+                onChange={props.changeHandler}
               />
             </Card>
           </li>
