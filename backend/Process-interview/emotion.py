96af2ef1ab241af181f341512babea061f482b01
mo1998 <42372125+mo1998@users.noreply.github.com>
2022-07-03 20:51:28 +0200

    Update emotion.py

--- backend/Process-interview/emotion.py
+++ backend/Process-interview/emotion.py
@@ -1,6 +1,6 @@
 from keras.models import load_model
 from time import sleep
-from keras.preprocessing.image import img_to_array
+from tensorflow.keras.utils import img_to_array
 from keras.preprocessing import image
 import cv2
 import numpy as np
